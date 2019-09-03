f950b3f0adb9c6708c20c307b2deee4f7ea3d62f
Kennethaustin <kenneth.austin93@gmail.com>
2019-09-03 15:50:54 -0400

    redo

---
+++ my-app/src/components/Dashboard.testing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Dashboard from './Dashboard';
+import '@testing-library/react/cleanup-after-each';
+import '@testing-library/jest-dom/extend-expect';
+
+describe('<Dashboard />', () => {
+    it("displays strikes", () => {
+        const { getByText } = render(<Dashboard />);
+        const strikes = getByText(/strike/i);
+
+        fireEvent.click(strikes);
+
+        expect(strikes);
+    })
+
+    it("displays balls", () => {
+        const { getByText } = render(<Dashboard />);
+        const balls = getByText(/ball/i);
+
+        fireEvent.click(balls);
+
+        expect(balls);
+        
+    })
+});
